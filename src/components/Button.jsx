b3f1ccc7c261de79cfd2b3fed2a0c1637fbca905
akhil <gollaakhil1115@gmail.com>
2023-12-13 23:18:27 -0600

    implemented subscribe section ui

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor, fullWidth}) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat
     text-lg leading-none
     ${backgroundColor ? `${backgroundColor}
     ${textColor} ${borderColor}` :
     
-    "bg-coral-red text-white border-coral-red" } rounded-full`}>
+    "bg-coral-red text-white border-coral-red" } rounded-full ${fullWidth && 'w-full'}"}`}>
 
       {label}
 
